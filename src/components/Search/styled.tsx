1933ecdd7448c040566842c6ada826a072a7c74b
Marcella Gutierrez <marcellagutierrez@MacBook-Pro-de-Marcella.local>
2022-12-27 16:41:27 -0300

    feat: search components

---
+++ src/components/Search/styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    width: 200px;
+`
+
+export const InputSearch = styled.input`
+    width: 100%;
+    border-radius: 5px;
+
+`;
