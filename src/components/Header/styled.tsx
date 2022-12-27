f9ad9cdf8b569fb5f3be7d5a9d5d1ebe92b74541
Marcella Gutierrez <marcellagutierrez@MacBook-Pro-de-Marcella.local>
2022-12-27 16:41:04 -0300

    feat: header component

---
+++ src/components/Header/styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 20px;
+`;
+
+export const Logo = styled.img`
+    width: 200px;
+`;
