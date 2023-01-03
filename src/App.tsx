721d9550bd8d4fb7c1582e4687b9dd4158610aef
Marcella Gutierrez <marcella.gutierrez@hotmail.comm>
2023-01-03 18:15:52 -0300

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
-import Router from "./router";
+import { ThemeProvider } from "styled-components"
+import React from 'react'
 
-function App() {
-  return <Router />;
+import { Outlet, Link } from "react-router-dom";
+import { FaSun, FaMoon } from 'react-icons/fa'
+
+import { light, dark } from './utils/styles/themes/Theme.styled'
+import { GlobalStyles } from './utils/styles/global'
+import {
+    Title,
+    ThemeSwitcher,
+    Wrapper,
+    Header
+} from './App.styled'
+
+export default function App() {
+    const [theme, setTheme] = React.useState(() => {
+        const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        return isDarkMode ? dark : light 
+    })
+    const toggleTheme = () => {
+        document.body.style.transition = "linear .2s";
+        (theme === light) ? setTheme(dark) : setTheme(light)
+    }
+    return (
+        <ThemeProvider theme={theme}>
+            <Wrapper>
+                <GlobalStyles />
+                <Header>
+                    <Link to="/"><Title>Pokedex</Title></Link>
+                    <ThemeSwitcher onClick={() => toggleTheme()}>
+                        {theme.title === "dark" ? (
+                            <FaSun color={theme.colors.primary} fontSize={30} />
+                        ) : (
+                            <FaMoon color={theme.colors.text} fontSize={30} />
+                        )}
+                    </ThemeSwitcher>
+                </Header>
+     
+                <Outlet/>
+            </Wrapper>
+        </ThemeProvider>
+    )
 }
-
-export default App;
