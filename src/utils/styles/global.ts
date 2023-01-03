3394af71ea16c518dc0b7bbf55a4f11e7ab5b6dd
Marcella Gutierrez <marcella.gutierrez@hotmail.comm>
2023-01-03 18:14:06 -0300

    feat: global styles

---
+++ src/utils/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    ::before,
+    ::after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        vertical-align: baseline;
+        list-style: none;
+        border: 0;
+    }
+    a {
+        text-decoration: none;
+    }
+    body {
+        background-color: ${({ theme }) => theme.colors.background};
+    }
+`
---
+++ src/utils/styles/themes/Theme.styled.ts
@@ -0,0 +1,27 @@
+export const dark = {
+    title: 'dark',
+    colors: {
+        primary: '#FFF58C',
+        primaryHover: '#FFEC00',
+        secundary: '#000',
+
+        background: '#121212',
+        backgroundCard: '#373737',
+        text: '#DFDFDF',
+        subtext: '#9E9E9E'
+    }
+}
+export const light = {
+    title: 'light',
+    colors: {
+        primary: '#80CEE1',
+        primaryHover: '#79D2E6',
+        secundary: '#000',
+
+        background: '#fff',
+        backgroundCard: '#F2F2F2',
+        text: '#313131',
+        subtext: '#919191'
+    }
+}
+
