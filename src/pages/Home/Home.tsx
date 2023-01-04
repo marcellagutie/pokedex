8bbcad0c5bfa7dab085010438397860912fd2652
Marcella Gutierrez <marcella.gutierrez@hotmail.comm>
2023-01-03 22:22:35 -0300

    fix: remove trash code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,45 +1,43 @@
-import React, {useEffect, Fragment} from 'react'
+import {useEffect, Fragment} from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 
-import { useInfiniteQuery } from 'react-query'
-import { Link } from 'react-router-dom'
-import { useInView } from 'react-intersection-observer'
+import PokemonCard from '../../components/PokeCard';
+import Filter from '../../components/Filter';
+import Loading from '../../components/Loading';
 
-import PokemonCard from '../../components/PokemonCard'
-import Filter from '../../components/Filter'
-import Loading from '../../components/Loading'
-
-import useApi from '../../services/api'
-import useHandles from '../../hooks/useHandles'
-import { PokemonList, InfinityScroll } from './Home.styled'
+import api from '../../services/api';
+import useHandle from '../../hooks/useHandle';
+import { PokemonList, InfinityScroll } from './Home.styled';
 
 const Home = () => {
-    const { infinityScroll } = useApi()
-    const { search, handleInputChange } = useHandles()
-    const { ref, inView } = useInView()
-    const {
-        data,
-        fetchNextPage,
-    } = useInfiniteQuery (
+    const { infinityScroll } = api();
+    const { filter, handleInputChange } = useHandle();
+    const { ref, inView } = useInView();
+    const {data, fetchNextPage } = useInfiniteQuery (
         'pokemons',
         ({pageParam = 0}) => infinityScroll(pageParam),
         { getNextPageParam: (page, all) => (page.length * all.length) }
     )
+
     useEffect(() => {
         if (inView) {
-            fetchNextPage()
+            fetchNextPage();
         }
-    }, [fetchNextPage, inView])
+    }, [fetchNextPage, inView]);
+
     return (
         <div>
-            <Filter handleInputChange={handleInputChange} value={search}/>
+            <Filter handleInputChange={handleInputChange} value={filter}/>
             <PokemonList>
             {data?.pages.map((page: any) => (
                 <Fragment key={`pokemon${page[0].id}`}>
                 {page.map((pokemon: any) => {
                     return (
                         <Link to={pokemon.name} key={pokemon.id.toString()}>
-                        <PokemonCard info={pokemon}/>
-                    </Link>
+                            <PokemonCard info={pokemon}/>
+                        </Link>
                     )
                 })}
                 </Fragment>
@@ -49,7 +47,7 @@ const Home = () => {
                 <Loading />
             </InfinityScroll>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
